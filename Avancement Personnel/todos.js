a6319f3a99a98fb7ee7be9d653cb57a9a97da06b
Axel Gonon <gohon95@gmail.com>
2020-03-01 12:28:12 +0100

    ajout de code

--- Avancement Personnel/todos.js
+++ Avancement Personnel/todos.js
@@ -1,25 +1,55 @@
-var vm = new Vue({
-    data() {
-        return {
-            todos: [{
-                name: 'Tache de test',
-                completed: false
-            }],
-            newTodo: ''
-        }
+var todos = new Vue({
+    el: "#todos",
+    data: {
+        todos: [],
+        newTodo: ''
     },
+
     methods: {
-        addTodo() {
+        // Ajouter une tâche
+        addTodo: function() {
             this.todos.push({
                 completed: false,
                 name: this.newTodo
             })
             this.newTodo = ''
+            console.log(this.todos)
+        },
+        // Supprimer une tâche
+        supTodo: function(todo) {
+            let name = todo.name
+            console.log(name)
+            this.todos = this.todos.filter(function(set) {
+                return set.name != name;
+            })
+        },
+        // Supprimer toutes les tâches accomplis
+        clearAccomplished: function() {
+            this.todos = this.todos.filter(function(set) {
+                return set.completed != true;
+            })
+        },
+        // je sais plus
+        allCompleted: function() {
+            let checked = event.target.checked
+            this.todos = this.todos.map(function(up) {
+                if(checked) {
+                    up.completed = true;
+                } else {
+                    up.completed = false;
+                }
+                return up;
+            })
         }
     },
     computed: {
-        remaining () {
-            this.todos.filter(todo => !todo.completed).lenght
+        // Compteur de tâche à faire
+        remaining: function () {
+            return this.todos.filter(todo => !todo.completed).length
+        },
+        // Compteur qui enlève une tâche à faire quand elle est complété
+        completed: function () {
+            return this.todos.filter(todo => todo.completed).length
         }
     }
 })
